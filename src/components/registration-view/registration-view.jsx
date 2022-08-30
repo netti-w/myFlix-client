a0a97f8de07766209f6091a036c82b7e9d1b52b2
netti-w <106514885+netti-w@users.noreply.github.com>
2022-08-30 13:39:47 +0200

    Create registration view component

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onRegistered(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birthday:
+        <input type="date" value={birthday} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Register</button>
+    </form>
+  );
+}
+
+RegistrationView.propTypes = {
+  register: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    birthday: PropTypes.date
+  }),
+  onRegistered: PropTypes.func.isRequired,
+};

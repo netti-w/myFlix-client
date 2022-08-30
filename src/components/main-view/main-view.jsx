b034d0ef8beddcfc2f13d419e74df45a08e03b8f
netti-w <106514885+netti-w@users.noreply.github.com>
2022-08-30 13:43:58 +0200

    Revert "Update"
    This reverts commit 7815faec8833773caa04eb15c3cfaeb09060471e.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-import { RegistrationView } from '../registration-view/registration-view';
-import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -10,12 +8,9 @@ class MainView extends React.Component {
 
   constructor() {
     super();
-    // Initial state is set to null
     this.state = {
       movies: [],
-      selectedMovie: null,
-      user: null,
-      registerUser: null
+      selectedMovie: null
     }
   }
 
@@ -31,37 +26,16 @@ class MainView extends React.Component {
       });
   }
 
-  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` 
-  *property to that movie*/
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
 
-  /* When a user successfully logs in, this function updates the `user` property in state 
-  to that *particular user*/
-  onLoggedIn(user) {
-    this.setState({
-      user
-    });
-  }
-
-  // onRegistered(registerUser) {
-  //   this.setState({
-  //     registerUser
-  //   });
-  // }
-
+  // condensed
   render() {
-    const { movies, selectedMovie, user } = this.state;
+    const { movies, selectedMovie } = this.state;
 
-    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-
-    // if (!registerUser) return <RegisterView onRegistered={user => this.onRegistered(registerUser)} />;
-
-    // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (

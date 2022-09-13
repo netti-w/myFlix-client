ceebad0154c77b5e0679cc50619bdec9c10d4800
netti-w <106514885+netti-w@users.noreply.github.com>
2022-09-13 12:22:07 +0200

    Fix dynamic content + delete redundant code

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-
-// import { Container, Button, Row, Col } from 'react-bootstrap';
-
-// import { Link } from "react-router-dom";
-
-// export class DirectorView extends React.Component {
-//     render() {
-//         const { movie, onBackClick } = this.props;
-
-//         return (
-//             <Container>
-//                 <Row className="movie-view" style={{ marginTop: 20, marginBottom: 20 }}>
-//                     <Col md={9} sm={6}>
-//                         <div className="director-name bg-dark-text" >
-//                             <h3 className="value"><span className="value">{movie.Director.Name}</span></h3>
-//                         </div>
-//                         <div className="director-birth bg-dark-text">
-//                             <span className="value">Born in {movie.Director.Birth}</span>
-//                         </div>
-//                         <div className="director bg-dark-text">
-//                             <span className="label">Biography: </span>
-//                             <span className="value">{movie.Director.Bio}</span>
-//                         </div>
-//                         <Button onClick={() => { onBackClick(null); }} variant="primary" style={{ marginTop: 15 }} className="button-primary px-4">Back</Button>
-//                     </Col>
-//                 </Row>
-//             </Container>
-//         );
-//     }
-// }
-
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
@@ -39,7 +7,7 @@ import { Container, Button, Row, Col, Card } from 'react-bootstrap';
 
 export class DirectorView extends React.Component {
   render() {
-    const { director, onBackClick, movies, movie } = this.props;
+    const { director, onBackClick } = this.props;
 
     return (
       <Container>
@@ -52,12 +20,10 @@ export class DirectorView extends React.Component {
               <span className="label">Born: </span>
               <span className="value">{director.Birth}</span>
             </div>
-            {/* if(director.Death) {
-                                <div className="director-birth bg-dark-text">
-                                    <span className="label">Died: </span>
-                                    <span className="value">{director.Death}</span>
-                                </div>
-                            }; */}
+            {director.Death ? (<div className="director-death bg-dark-text">
+              <span className="label">Died: </span>
+              <span className="value">{director.Death}</span>
+            </div>) : (<div />)}
             <div className="director-bio bg-dark-text">
               <span className="label">Biography: </span>
               <span className="value">{director.Bio}</span>
